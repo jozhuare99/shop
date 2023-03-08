ddd8432ea6ea17bebc515aa646387bbd1cea41ba
jozhuare99 <jozhuare99@gmail.com>
2023-03-09 07:21:40 +0800

    new task is fix the vs code expensive memory

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -2,7 +2,6 @@ import React,   {useState} from 'react'
 import {Nav, Container, Form, Button, InputGroup} from 'react-bootstrap'
 
 
-
 export default function Header() {
   return (
     <>
@@ -11,13 +10,13 @@ export default function Header() {
                 <Nav.Item>
                 <Form className="d-flex">
                 <InputGroup className="mb-3">
-                <InputGroup.Text id="basic-addon1">@</InputGroup.Text>
+                <i className="bi bi-search"></i>
                 <Form.Control
                 placeholder="Username"
                 aria-label="Username"
                 aria-describedby="basic-addon1"
                 />
-                <i className="bi bi-bag-fill">test etsttsts</i>
+                <i className="bi bi-bag-fill">test</i>
                 </InputGroup>
                   <Button variant="outline-success">Search</Button>
                 </Form>
