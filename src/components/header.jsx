2dbbdbdd95814bd84eb0a6faee73e9bbba64543c
jozhuare99 <jozhuare99@gmail.com>
2023-03-09 15:58:12 +0800

    setup all needed

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -10,7 +10,8 @@ export default function Header() {
      <Nav.Item>
       <Form className="d-flex ">
       <InputGroup className="mb-3 text-input-group-search pe-3 d-flex justify-content-center align-content-center">
-          <InputGroup.Text className='bg-white border-end-0 border-dark pe-1'><i className="bi bi-search text-search-icon"></i></InputGroup.Text>
+          <InputGroup.Text className='bg-white border-end-0 border-dark pe-1'><i className="bi bi-search text-search-icon "></i></InputGroup.Text>
+          
       <Form.Control placeholder="Username" aria-label="Username" aria-describedby="basic-addon1" className='textInputSearch rounded-end border-start-0 border-dark pe-5 text-dark bg-primary'/>
         <Button className='text-dark bg-white border-dark m-2 rounded-start position-absolute '>Search</Button>
       </InputGroup>
