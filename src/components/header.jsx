693b6c8a282f07752e7f6a4151cfdb740222f4ac
jozhuare99 <jozhuare99@gmail.com>
2023-03-09 22:08:50 +0800

    search fixed

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -4,25 +4,32 @@ import {Nav, Container, Form, Button, InputGroup} from 'react-bootstrap'
 
 export default function Header() {
  return (
-  <> 
-   <Container className='mt-2'>
-    <Nav variant='pills' defaultActiveKey={'/'}>
-     <Nav.Item>
-      <Form className="d-flex ">
-      <InputGroup className="mb-3 text-input-group-search pe-3 d-flex justify-content-center align-content-center">
-          <InputGroup.Text className='bg-white border-end-0 border-dark pe-1'><i className="bi bi-search text-search-icon "></i></InputGroup.Text>
-          
-      <Form.Control placeholder="Username" aria-label="Username" aria-describedby="basic-addon1" className='textInputSearch rounded-end border-start-0 border-dark pe-5 text-dark bg-primary'/>
-        <Button className='text-dark bg-white border-dark m-2 rounded-start position-absolute '>Search</Button>
+  <>
+   <Container className='my-2 headerMaxHeight'>
+    <Nav variant='' defaultActiveKey={'/'} className='headerMaxHeight p-0'>
+     <Nav.Item className="p-0 m-0">
+      <Form className="p-0 m-0">
+      <InputGroup className="p-0 m-0 text-input-group-search d-flex justify-content-end align-content-center">
+          <InputGroup.Text className='bg-white border-end-0 border-dark pe-1  '><i className="bi bi-search text-search-icon my-0 "></i></InputGroup.Text>
+      <Form.Control placeholder="Samsung Galaxy" aria-label="Username" aria-describedby="basic-addon1" className='textInputSearch rounded-end border-start-0 border-dark py-0 my-0 text-dark'/>
+        <Button className='btn-search text-dark bg-white  my-auto border-dark rounded-start'>Search</Button>
       </InputGroup>
-      <i className="bi bi-bag-fill"></i>
       </Form>
       </Nav.Item>
-      <Nav.Item>
-       <Nav.Link href='/shop'>
-              
-        </Nav.Link>
+      <Nav.Item className='user-select-none'>
+        <Container  className='h-100 cartBagContainer px-2'>
+        <a role='button' onClick={()=>console.log('clicked')} className=' d-flex text-decoration-none align-items-center text-center justify-content-center'>
+          <i className="text-cart-bag text-dark bi bi-bag-fill"></i>
+          <p className='cartNumberCount text-white  position-absolute mx-auto mt-4 fs-4'>9</p>
+          </a>
+        </Container>
       </Nav.Item>
+      <Nav.Item>
+        <div className='profileContainer'>
+          <i className="text-profile-icon text-dark bi bi-person-circle"></i>
+        </div>
+      </Nav.Item>
+
      </Nav>
   </Container>
  </>
---
+++ src/components/navigationLinkData.jsx
@@ -0,0 +1,9 @@
+
+
+const initialData = [
+{id: 2, name:  'home', icon: 'house-fill'},
+{id: 3, name:  'flash', icon: 'flash-fill'},
+{id: 4, name:  'feed', icon: 'heart-fill'},
+{id: 5, name:  'popular', icon: 'fire-fill'},
+{id: 6, name:  'about', icon: 'questionmark-fill'}
+]
