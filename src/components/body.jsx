a2910e42a5f8acc043d982016ade6523e21f1d10
jozhuare99 <jozhuare99@gmail.com>
2023-03-11 23:10:20 +0800

    noth

--- src/components/body.jsx
+++ src/components/body.jsx
@@ -3,44 +3,41 @@ import {Button, Card} from 'react-bootstrap'
 import {Container} from 'react-bootstrap'
 import axios from 'axios'
 
-// const api = axios.create({
-//   baseURL:'https://api.ninja.com',
-//   responseType: 'blob',
-//   headers:{
-//     'X-Api-Key':'OnlrIUXF/WMF3pxax/Nvvw==wJbnPxW0AZ8EvgeY'
-//   }
-// })
 
 export default function Body(){
 	const [image, setImage] = useState(null)
 
 
 
-  useEffect(()=>{
-    const category = 'nature'
+  // useEffect(()=>{
+  //   const category = 'nature'
 
-    axios.get('https://api.api-ninjas.com/v1/randomimage?category='+category,{
-      responseType: 'blob',
-        headers:{
-    'X-Api-Key':'OnlrIUXF/WMF3pxax/Nvvw==wJbnPxW0AZ8EvgeY',
-    'Accept': 'image/jpg'
-  }
+  //   axios.get('https://api.api-ninjas.com/v1/randomimage?category='+category,{
+  //     responseType: 'blob',
+  //       headers:{
+  //   'X-Api-Key':'OnlrIUXF/WMF3pxax/Nvvw==wJbnPxW0AZ8EvgeY',
+  //   'Accept': 'image/jpg'
+  // }
 
-    })
-    .then(response=>{
-      const imageUrl = URL.createObjectURL(response.data)
-      setImage(imageUrl)
-    })
-    .catch(error=>{
-      console.error(error)
-    })
-  }, [])
+  //   })
+  //   .then(response=>{
+  //     const imageUrl = URL.createObjectURL(response.data)
+  //     setImage(imageUrl)
+  //   })
+  //   .catch(error=>{
+  //     console.error(error)
+  //   })
+  // }, [])
 
 	return (
 		<Container className={'shopBody'}>
-			 <Card style={{ width: '18rem' }}>
-        <Card.Img variant="top" src={image!==null ? image: 'https://th.bing.com/th/id/OIP.cmTcUqX-2WFqJofdiwQLBQHaHa'}/>
-        <Card.Body>
+      <Container >
+			 <Card className='mx-2'>
+        <Card.Img variant="top" src={'https://mobiili.fi/wp-content/uploads/2020/11/iphone_12_mini_pre_2-800x675.jpg'}/>
+        <div className='bg-warning my-0 card-rating'>
+          <p className='my-0 ms-1'>start star</p>
+        </div>
+        <Card.Body className='pt-1'>
         <Card.Title>Card Title</Card.Title>
         <Card.Text>
           Some quick example text to build on the card title and make up the
@@ -49,18 +46,9 @@ export default function Body(){
         <Button variant="primary">Go somewhere</Button>
         </Card.Body>
       </Card>
-       <Card style={{ width: '18rem' }}>
-        <Card.Img variant="top" src={image!==null ? image: 'https://th.bing.com/th/id/OIP.cmTcUqX-2WFqJofdiwQLBQHaHa'}/>
-        <Card.Body>
-        <Card.Title>Card Title</Card.Title>
-        <Card.Text>
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
-        </Card.Text>
-        <Button variant="primary">Go somewhere</Button>
-        </Card.Body>
-      </Card>
-      
-		</Container>
-		)
+     
+
+		`</Container>
+    </Container>
+	)
 } 
