e17e34754dfb56d21c06d86ce2422ce4478f322e
jozhuare99 <jozhuare99@gmail.com>
2023-03-14 21:22:55 +0800

    body fix

--- src/components/body.jsx
+++ src/components/body.jsx
@@ -1,8 +1,7 @@
 import { useState, useEffect } from 'react'
-import { Button, Card } from 'react-bootstrap'
+import { Card } from 'react-bootstrap'
 import { Container } from 'react-bootstrap'
 import {v1} from 'uuid'
-// import axios from 'axios'
 
 
 export default function Body() {
@@ -11,63 +10,42 @@ export default function Body() {
   let emptyValue =  [
     {id: 0, name: '', price: 0, percentDiscount: 0, rating: 0, sold: 0, reviewed: 0, location: '', imgLink: ''},
   ]
-  const [items1, setItems1] = useState(emptyValue)
-  const [items2, setItems2] = useState(emptyValue)
-  const [items3, setItems3] = useState(emptyValue)
-  const [items4, setItems4] = useState(emptyValue)
-  const [items5, setItems5] = useState(emptyValue)
+  const [items, setItems] = useState(emptyValue)
 
   useEffect(()=>{
     function  createItem(id, name, price, percentDiscount,rating,sold,reviewed,location,imgLink){
       return {id, name, price, percentDiscount,rating,sold,reviewed,location,imgLink}
     }
-     let initialItem = [
-      createItem(0,'Apple iPhone 14',54390,12,80,20,10,'Metro Manila','https://lzd-img-global.slatic.net/g/p/c3a87a020e49a43809142e7bef034494.jpg_720x720q80.jpg_.webp'),
-      createItem(1,'Samsung Galaxy s23 8GB+128GB/256/Galaxy s23+ 8GB +256GB/512GB Dual Sim 5G',42990 ,20 ,100 ,2 ,1 ,'Bulacan','https://lzd-img-global.slatic.net/g/p/d72700f9ce5d7ff78a2cf574d5687ed1.png_720x720q80.png_.webp'),
-      createItem(2,'Xiaomi 13 12+256 Global Version [in 1+1 year warranty]',44999 ,0 ,76 ,300 ,215 ,'Overseas','https://lzd-img-global.slatic.net/g/p/e0dcae74dd5cd99e39cf26378011298d.png_720x720q80.png_.webp'),
-      createItem(3,'Google Pixel 6 PRO 5G 12GB/128GB [Unlocked] with Free Google Pixel Buds',34990 ,25 ,50 ,2 ,1 ,'Metro Manila','https://lzd-img-global.slatic.net/g/p/7045863a9aedd2c1e00d5fc2da333b97.png_720x720q80.png_.webp'),
-      createItem(4,'Nokia 5710 XpressAudio, Unisoc T107, 128MB + 48MB, Removable 1450 mAh Battery, Cellphone',4190 ,0 ,62 ,100 ,85 ,'Metro Manila','https://lzd-img-global.slatic.net/g/p/87a76cefdb37037f1b1036ff349cf8b0.png_720x720q80.png_.webp')
+
+    function createItems(data){
+      let items = []
+      for(let subarray of data){
+        let item = createItem(...subarray)
+        items.push(item)
+      }
+      return items;
+    }
+     let data = [
+      [0,'Apple iPhone 14',54390,12,80,20,10,'Metro Manila','https://lzd-img-global.slatic.net/g/p/c3a87a020e49a43809142e7bef034494.jpg_720x720q80.jpg_.webp'],
+      [45,'Apple iPhone 14',54390,12,80,20,10,'Metro Manila','https://lzd-img-global.slatic.net/g/p/c3a87a020e49a43809142e7bef034494.jpg_720x720q80.jpg_.webp'],
+      [1,'Samsung Galaxy s23 8GB+128GB/256/Galaxy s23+ 8GB +256GB/512GB Dual Sim 5G',42990 ,20 ,100 ,2 ,1 ,'Bulacan','https://lzd-img-global.slatic.net/g/p/d72700f9ce5d7ff78a2cf574d5687ed1.png_720x720q80.png_.webp'],
+      [2,'Xiaomi 13 12+256 Global Version [in 1+1 year warranty]',44999 ,0 ,76 ,300 ,215 ,'Overseas','https://lzd-img-global.slatic.net/g/p/e0dcae74dd5cd99e39cf26378011298d.png_720x720q80.png_.webp'],
+      [3,'Google Pixel 6 PRO 5G 12GB/128GB [Unlocked] with Free Google Pixel Buds',34990 ,25 ,50 ,2 ,1 ,'Metro Manila','https://lzd-img-global.slatic.net/g/p/7045863a9aedd2c1e00d5fc2da333b97.png_720x720q80.png_.webp'],
+      [4,'Nokia 5710 XpressAudio, Unisoc T107, 128MB + 48MB, Removable 1450 mAh Battery, Cellphone',4190 ,0 ,62 ,100 ,85 ,'Metro Manila','https://lzd-img-global.slatic.net/g/p/87a76cefdb37037f1b1036ff349cf8b0.png_720x720q80.png_.webp']
      ]
+     let initialItems = createItems(data)
+     
 
-     function createSets(items) {
-      let sets = []
-      for (let i = 0; i < 5; i++){
-        sets[i] = []
-      }
-      for (let j = 0; j < items.length; j++){
-        let index = j % 5;
-        sets[index].push(items[j])
-      }
-      return sets;
-     }
 
-     let [item1,item2,item3,item4,item5] = createSets(initialItem)
-
-    setItems1(item1)
-    setItems2(item2)
-    setItems3(item3)
-    setItems4(item4)
-    setItems5(item5)
+    setItems(initialItems)
   },[])
 
   return (
     <>
-      <Container className='user-select-none  shopBody d-flex flex-wrap align-items-start justify-content-start mx-auto'>
-          <div className='card-item d-flex flex-column mx-auto'>
-            <Items items={items1} />
-          </div>
-          <div className='card-item  d-flex flex-column mx-auto'>
-            <Items items={items2} />
-          </div>
-          <div className='card-item d-flex flex-column '>
-            <Items items={items3} />
-          </div>
-          <div className='card-item d-flex flex-column '>
-            <Items items={items4} />
-          </div>
-          <div className='card-item d-flex flex-column '>
-            <Items items={items5} />
-          </div>
+      <Container className='user-select-none shopBody mx-auto'>
+         <div className='d-flex flex-wrap flex-row w-100'>
+         <Items items={items} />
+         </div>
       </Container>
     </>
   )
@@ -78,7 +56,7 @@ function Items({items}){
   return (
     <>
       {items.map(item=>
-        item.name ? <Card className='my-2 mx-1 h-100' key={item.id}>
+        item.name ? <Card className='card-item mt-3 mx-auto' key={v1()}>
         <Card.Img variant="top" src={item.imgLink} />
         <div className=' my-0 card-rating'>
           <p className='my-0 ms-1 text-small'>
@@ -111,7 +89,7 @@ function Items({items}){
           </p>
         </div>
         <Card.Body className=' p-2'>
-          <Card.Title className='mb-0  text-smaller '>{item.name}</Card.Title>
+          <Card.Title className='mb-0  text-smaller '>{item.name.length > 22 ? item.name.slice(0,19)+'...': item.name}</Card.Title>
           <Card.Text className='m-0 text-large p-0'>&#8369;{item.price}</Card.Text>
           <p className=' text-tiny mb-0'>{item.percentDiscount}% discount off &#8369;{item.price * (item.percentDiscount/100)}</p>
           <p className='text-meduim mb-0'>{item.sold} SOLD</p>
@@ -130,24 +108,5 @@ let initialdata = [
   {id: 1, name: 'Samsung Galaxy s23 8GB+128GB/256/Galaxy s23+ 8GB +256GB/512GB Dual Sim 5G', price: 42990, percentDiscount: 20, reviewed: 1, rating: 100, sold: 2, location: 'Bulacan', imgLink: 'https://lzd-img-global.slatic.net/g/p/d72700f9ce5d7ff78a2cf574d5687ed1.png_720x720q80.png_.webp'},
   {id: 2, name: 'Xiaomi 13 12+256 Global Version [in 1+1 year warranty]', price: 44999, percentDiscount: 0, rating: 76, sold: 300, reviewed: 215,  location: 'Overseas', imgLink: 'https://lzd-img-global.slatic.net/g/p/e0dcae74dd5cd99e39cf26378011298d.png_720x720q80.png_.webp'},
   {id: 3, name: 'Google Pixel 6 PRO 5G 12GB/128GB [Unlocked] with Free Google Pixel Buds', price: 34990, percentDiscount: 25, rating: 50, sold: 2, reviewed: 1,  location: 'Metro Manila', imgLink: 'https://lzd-img-global.slatic.net/g/p/7045863a9aedd2c1e00d5fc2da333b97.png_720x720q80.png_.webp'},
-  {id: 4, name: 'Nokia 5710 XpressAudio, Unisoc T107, 128MB + 48MB, Removable 1450 mAh Battery, Cellphone', price: 4190, percentDiscount: 0, rating: 62, sold: 100, reviewed: 85,  location: 'Metro Manila', imgLink: 'https://lzd-img-global.slatic.net/g/p/87a76cefdb37037f1b1036ff349cf8b0.png_720x720q80.png_.webp'},
-]
-
-let initialItems1 = [
-  {id: 0, name: 'Apple iPhone 14', price: 54390, percentDiscount: 12, rating: 80, sold: 20, reviewed: 10, location: 'Metro Manila', imgLink: 'https://lzd-img-global.slatic.net/g/p/c3a87a020e49a43809142e7bef034494.jpg_720x720q80.jpg_.webp'},
-]
-let initialItems2 = [
-  {id: 1, name: 'Samsung Galaxy s23 8GB+128GB/256/Galaxy s23+ 8GB +256GB/512GB Dual Sim 5G', price: 42990, percentDiscount: 20, reviewed: 1, rating: 100, sold: 2, location: 'Bulacan', imgLink: 'https://lzd-img-global.slatic.net/g/p/d72700f9ce5d7ff78a2cf574d5687ed1.png_720x720q80.png_.webp'},
-]
-
-let initialItems3 = [
-  {id: 2, name: 'Xiaomi 13 12+256 Global Version [in 1+1 year warranty]', price: 44999, percentDiscount: 0, rating: 76, sold: 300, reviewed: 215,  location: 'Overseas', imgLink: 'https://lzd-img-global.slatic.net/g/p/e0dcae74dd5cd99e39cf26378011298d.png_720x720q80.png_.webp'},
-]
-
-let initialItems4 = [
-  {id: 3, name: 'Google Pixel 6 PRO 5G 12GB/128GB [Unlocked] with Free Google Pixel Buds', price: 34990, percentDiscount: 25, rating: 50, sold: 2, reviewed: 1,  location: 'Metro Manila', imgLink: 'https://lzd-img-global.slatic.net/g/p/7045863a9aedd2c1e00d5fc2da333b97.png_720x720q80.png_.webp'},
-]
-
-let initialItems5 = [
   {id: 4, name: 'Nokia 5710 XpressAudio, Unisoc T107, 128MB + 48MB, Removable 1450 mAh Battery, Cellphone', price: 4190, percentDiscount: 0, rating: 62, sold: 100, reviewed: 85,  location: 'Metro Manila', imgLink: 'https://lzd-img-global.slatic.net/g/p/87a76cefdb37037f1b1036ff349cf8b0.png_720x720q80.png_.webp'},
 ]
