c739d2d54aec72510798b0106733a6553f0ab66c
jozhuare99 <jozhuare99@gmail.com>
2023-03-21 13:11:03 +0800

    adding chat

--- src/components/chat.jsx
+++ src/components/chat.jsx
@@ -3,54 +3,58 @@ import { Container, Stack, Card, Button, Form, InputGroup } from 'react-bootstra
 
 
 export default function Chat(){
-
+  const [showMessage, setShowMessage] = useState(false)
   return(
     <>
     <Container className='position-relative'>
       <div className='float-chat flip-horizontaly position-absolute'>
-        <i role='button' onClick={()=>console.log('message open')} className='fs-1 bi bi-chat-fill text-black'></i>
+        <i role='button' onClick={()=>setShowMessage(!showMessage)} className='fs-1 bi bi-chat-fill text-black'></i>
       </div>
-      <ChatBox/>
+      <ChatBox showMessage={showMessage}/>
     </Container>
     </>
   )
 }
 
-function ChatBox(){
+function ChatBox({showMessage}){
   const [message, setMessage] = useState('Hello')
 
+
   return (
-  <div className='chat-box d-flex position-absolute bg-opacity-25 bg-warning'>
-      <Stack gap={2} className='justify-content-center row'>
+      showMessage && 
+      <div className='chat-box  d-flex position-absolute'>
+          <Stack gap={2} className='bottom'>
 
-          <Card className=''>
-            <Card.Body>
-              <Stack className='flex-row justify-content-start mb-4'>
-                <i role='button' className='text-profile-icon text-dark fs-3  mx-auto bi bi-shop'></i>
-                <div className='p-3 ms-3 border' style={{borderRaduis: '15px', backgroundColor:'white'}}>
-                  <p className=' small mb-0'>Hello and Thank you for visiting our Shop.</p>
-                </div>
-              </Stack>
+              <Card className='bg-transparent border-0 chat-box-size overflow-auto'>
+                <Card.Body>
+                  <Stack className='flex-row justify-content-start mb-4'>
+                    <i role='button' className='text-profile-icon text-dark fs-3  mx-auto bi bi-shop'></i>
+                    <div className='p-2 ms-3 border rounded-2 bg-light' >
+                      <p className=' small mb-0'>Hello and Thank you for visiting our Shop. hsajdhaksdhbash aksdhg iaysg dguyask d</p>
+                    </div>
+                  </Stack>
 
-              <Stack className='flex-row justify-content-end mb-4'>
-                <div className='p-3 ms-3 border' style={{borderRaduis: '15px', backgroundColor:'white'}}>
-                  <p className=' small mb-0'>Hi how to order?</p>
-                </div>
-                <i role='button' className="text-profile-icon pe-1 text-dark fs-3 bi bi-person-circle"></i>
-              </Stack>
-              
-            </Card.Body>
-            <Card.Footer>
-              <Form className="p-0 m-0">
-                <InputGroup className="text-input-group-search d-flex justify-content-end align-content-center">
-                 <Form.Control placeholder="Samsung Galaxy" aria-label="Username" aria-describedby="basic-addon1" className='textInputSearch rounded border-dark py-0 my-0 text-dark'/>
-                <Button className='btn-search text-dark bg-white  my-auto border-dark rounded-start'>Search</Button>
-              </InputGroup>
-              </Form>
-            </Card.Footer>
-          </Card>
-      </Stack>
-  </div>
+                  <Stack className='flex-row justify-content-end mb-4'>
+                    <div className='p-2 border rounded-2 bg-light me-2'>
+                      <p className=' small mb-0'>Hi how to order?</p>
+                    </div>
+                    <i role='button' className="user-profile-icon text-dark fs-3 bi bi-person-circle"></i>
+                  </Stack>
+                  
+                  <Form className="p-0 m-0">
+                    <Form.Group className="message-text d-flex justify-content-around align-items-end">
+                      <Form.Control as="textarea" rows={2} name='chat-message' className='text-message-chat border-dark  me-2'/>
+                      <Button className='btn-chat-send text-dark bg-white border-dark '>Send</Button>
+                    </Form.Group>
+                  </Form>
+
+                </Card.Body>
+            
+                
+            
+              </Card>
+          </Stack>
+      </div>
   )
 
 }
