c9e062a3769bc2f5602954e733873ba1dd5aedce
jozhuare99 <jozhuare99@gmail.com>
2023-05-24 16:36:36 +0800

    add

--- src/components/api/auth.js
+++ src/components/api/auth.js
@@ -10,48 +10,48 @@ const SampleAuthProvider = {
     }
 }
 
-const makeAuthenticatedRequest = async (url, method, body) => {
-    const user = await User.findOne({ username: localStorage.getItem("username") })
-    const password = localStorage.getItem("password")
+// const makeAuthenticatedRequest = async (url, method, body) => {
+//     const user = await User.findOne({ username: localStorage.getItem("username") })
+//     const password = localStorage.getItem("password")
 
-    const base64Credentials = Buffer.from(`${user.username}:${password}`).toString("base64")
+//     const base64Credentials = Buffer.from(`${user.username}:${password}`).toString("base64")
 
-    const request = {
-        method,
-        headers: {
-            "Authorization": `Basic ${base64Credentials}`
-        },
-        body, 
-    }
+//     const request = {
+//         method,
+//         headers: {
+//             "Authorization": `Basic ${base64Credentials}`
+//         },
+//         body, 
+//     }
 
-    const response = await fetch(url, request)
+//     const response = await fetch(url, request)
 
-    if(response.status !== 200){
-        throw new Error(`Request failed with status code ${response.status}`)
-    }
+//     if(response.status !== 200){
+//         throw new Error(`Request failed with status code ${response.status}`)
+//     }
 
-    return await response.json();
-}
+//     return await response.json();
+// }
 
 
-const response = await fetch('/login', {
-    method: 'POST',
-    body: JSON.stringify({
-      username: 'username',
-      password: 'password',
-    }),
-  });
+// const response = await fetch('/login', {
+//     method: 'POST',
+//     body: JSON.stringify({
+//       username: 'username',
+//       password: 'password',
+//     }),
+//   });
   
-  const data = await response.json();
+//   const data = await response.json();
   
-  if (data.success) {
-    // The login was successful
-  } else {
-    // The login failed
-  }
+//   if (data.success) {
+//     // The login was successful
+//   } else {
+//     // The login failed
+//   }
   
 
 
 
-export { SampleAuthProvider,makeAuthenticatedRequest };
+export { SampleAuthProvider };
 
